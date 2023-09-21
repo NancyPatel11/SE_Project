e3c396aff88ae35b98f49ae039f9edf8936eeed4
Nancy Patel <nancypatel5757@gmail.com>
2023-09-21 15:06:25 +0530

    updated homepage inline-css

--- Login-Page/src/Components/ProjectHome.jsx
+++ Login-Page/src/Components/ProjectHome.jsx
@@ -16,22 +16,22 @@ function ProjectHome() {
                 <div className="col-9">
                     <div className="container"></div>
                     <div className="row">
-                        <div className="col-12 my-3 py-3" style={{ color: "#fff", backgroundColor: "#304D6D", borderRadius: "15px" }}>
+                        <div className="col-12 my-3 py-3 text-white rounded-4" style={{backgroundColor: "#304D6D"}}>
                             <h2 className='display-3 fw-semibold'>Project Name</h2>
                             <p>Project Description Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus labore maxime quisquam sapiente ratione nemo aliquid numquam tempore repellendus voluptatum!</p>
                         </div>
                         <div className="w-100"></div>
-                        <div className="col-md-5 shadow-lg" style={{backgroundImage: `url(${bg1})`, backgroundPosition: "center", backgroundSize: "cover", borderRadius: "15px", width:500 }}>
-                            <h3 className='mx-3 my-3' style={{color: "#fff", fontWeight: "700", fontSize: "2.5rem" }}>Total Budget</h3>
-                            <h6 className='mx-3' style={{ color: "#fff", fontWeight: "700", fontSize: "2rem" }}>1000000</h6>
+                        <div className="col-md-5 shadow-lg rounded-4" style={{ backgroundImage: `url(${bg1})`, backgroundPosition: "center", backgroundSize: "cover"}}>
+                            <h3 className='mx-3 my-3 fw-bold display-6 text-white'>Total Budget</h3>
+                            <h6 className='mx-3 fw-semibold h2 text-light'>1000000</h6>
                             <div className="pie">
-                            <TotalExpPie />
+                                <TotalExpPie />
                             </div>
                         </div>
-                        <div className="mx-4 col-md-5 py-2 shadow-lg" style={{ backgroundColor: "#B3C8E4", borderRadius: "15px", width:500}}>
-                            <h3 className="text-center py-2" style={{ color: "#fff", backgroundColor: "#295CAA", borderRadius: "15px" }}>Employees</h3>
-                            <div className="overflow-auto scroll custom-scroll-container" style={{ maxHeight: "350px"}}>
-                                <ol className="list-group list-group-numbered" style={{ maxWidth: "100%"}}>
+                        <div className="mx-4 col-md-5 py-2 shadow-lg rounded-4" style={{ backgroundColor: "#B3C8E4" }}>
+                            <h3 className="text-center py-2 rounded-3 text-white" style={{ backgroundColor: "#295CAA" }}>Employees</h3>
+                            <div className="overflow-auto scroll custom-scroll-container" style={{ maxHeight: "350px" }}>
+                                <ol className="list-group list-group-numbered">
                                     <li className="list-group-item">Employee 1</li>
                                     <li className="list-group-item">Employee 2</li>
                                     <li className="list-group-item">Employee 3</li>
@@ -52,24 +52,24 @@ function ProjectHome() {
                         </div>
 
                         <div className="w-100 my-2"></div>
-                        <div className="col-md-5 shadow-lg" style={{ backgroundImage: `url(${bg2})`, backgroundPosition: "center", backgroundSize: "cover", borderRadius: "15px", height:"425px", width:500}}>
-                            <h3 className='mx-3 my-3' style={{ color: "#fff", fontWeight: "700", fontSize: "2.5rem" }}>My Expense</h3>
-                            <h6 className='mx-3' style={{ color: "#fff", fontWeight: "700", fontSize: "2rem" }}>200</h6>
+                        <div className="col-md-5 shadow-lg rounded-4" style={{ backgroundImage: `url(${bg2})`, backgroundPosition: "center", backgroundSize: "cover" }}>
+                            <h3 className='mx-3 my-3 fw-bold display-6 text-white' >My Expense</h3>
+                            <h6 className='mx-3 fw-semibold h2 text-light'>200</h6>
                             <div className="pie">
-                            <MyExpPie />
+                                <MyExpPie />
                             </div>
                         </div>
-                        <div className="col-md-5 mx-4 shadow-lg" style={{ backgroundImage: `url(${bg3})`, backgroundPosition: "center", backgroundSize: "cover", borderRadius: "15px" , width:500}}>
+                        <div className="col-md-5 mx-4 py-2 shadow-lg rounded-4" style={{ backgroundImage: `url(${bg3})`, backgroundPosition: "center", backgroundSize: "cover"}}>
                             <div className="container my-3">
                                 <div className="row justify-content-md-center">
-                                    <div className="col-9 my-5" style={{ color: "#fff", backgroundColor: "#184FA3", borderRadius: "30px" }}>
+                                    <div className="col-9 my-5 py-2 text-white rounded-5" style={{backgroundColor: "#184FA3"}}>
                                         <h3 className='mx-3 my-1'>Approved Expenses</h3>
-                                        <p className='mx-3' style={{ fontSize: "30px", fontWeight: "500" }}>2</p>
+                                        <p className='mx-3 h3'>2</p>
                                     </div>
                                     <div className="w-100"></div>
-                                    <div className="col-9 my-4" style={{ color: "#000", backgroundColor: "#C7C7C7", borderRadius: "30px" }}>
+                                    <div className="col-9 my-4 mb-5 py-2 rounded-5" style={{backgroundColor: "#C7C7C7" }}>
                                         <h3 className='mx-3 my-1'>Pending Approval</h3>
-                                        <p className='mx-3' style={{ fontSize: "30px", fontWeight: "500" }}>2</p>
+                                        <p className='mx-3 h3'>2</p>
                                     </div>
                                 </div>
                             </div>
